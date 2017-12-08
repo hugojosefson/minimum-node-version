1a9f3b2ab8d2178d28d6219d84633ea3c07c2d7b
Hugo Josefson <hugo@josefson.org>
2017-12-08 23:51:19 +0100

    Use semver.minSatisfying().

--- src/minimum-node-version.mjs
+++ src/minimum-node-version.mjs
@@ -1,4 +1,4 @@
-import {satisfies, clean, compare} from 'semver'
+import {clean, minSatisfying} from 'semver'
 
 import versionData from './version-data'
 import expectedNodeVersion from 'expected-node-version'
@@ -6,13 +6,12 @@ import expectedNodeVersion from 'expected-node-version'
 export default () => {
   const expected = expectedNodeVersion()
 
-  return versionData()
-      .then(
-          records => records
-          .filter(record => record.name === 'Node.js')
-              .filter(record => satisfies(record.version, expected))
-              .map(record => record.version)
-              .map(version => clean(version))
-              .sort(compare)[0]
-      )
+  return versionData().then(records => {
+    const versions = records
+            .filter(record => record.name === 'Node.js')
+            .map(record => record.version)
+            .map(version => clean(version))
+
+    return minSatisfying(versions, expected)
+  })
 }
